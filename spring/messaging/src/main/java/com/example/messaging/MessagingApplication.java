dcbd1719e896842efc9b958026f59d51a7c3d811
Mario Gray <mario.gray@gmail.com>
2017-11-19 22:51:56 -0800

    add messaging demo

---
+++ spring/messaging/src/main/java/com/example/messaging/MessagingApplication.java
@@ -0,0 +1,71 @@
+package com.example.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.stereotype.Component;
+
+import javax.jms.ConnectionFactory;
+
+@SpringBootApplication
+@EnableJms
+@Log
+public class MessagingApplication {
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(MessagingApplication.class, args);
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+        log.info("Sending an email");
+        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+
+    }
+}
+
+@Component
+@Log
+class Receiver {
+
+    @JmsListener(id = "mailbox", destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        log.info("Received <" + email + ">");
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Email {
+    String to;
+    String body;
+}
