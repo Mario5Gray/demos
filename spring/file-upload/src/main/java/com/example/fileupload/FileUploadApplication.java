4b81958b48aceaeab682f06abcce139944cff7c0
Mario Gray <mario.gray@gmail.com>
2017-11-20 17:18:59 -0800

    file-upload demo

---
+++ spring/file-upload/src/main/java/com/example/fileupload/FileUploadApplication.java
@@ -0,0 +1,25 @@
+package com.example.fileupload;
+
+import hello.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
