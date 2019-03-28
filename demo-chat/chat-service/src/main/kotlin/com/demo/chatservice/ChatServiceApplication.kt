d680a943a60b82a00dcd74eed89da83f3250c57a
Mario Gray <mario.gray@gmail.com>
2019-03-28 12:43:34 +0100

    simple

--- demo-chat/chat-service/src/main/kotlin/com/demo/chatservice/ChatServiceApplication.kt
+++ demo-chat/chat-service/src/main/kotlin/com/demo/chatservice/ChatServiceApplication.kt
@@ -1,22 +1,11 @@
 package com.demo.chatservice
 
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.data.cassandra.repository.query.CassandraEntityMetadata
 
 
 @SpringBootApplication
-class ChatServiceApplication {
-    @Bean
-    fun appRun(context: ApplicationContext) = ApplicationRunner {
-        val metadata = context.getBeanNamesForType(CassandraEntityMetadata::class.java)
-        System.out.println("IAMRUN")
-        metadata.forEach { System.out.println("METADATA: $it") }
-    }
-}
+class ChatServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<ChatServiceApplication>(*args)
