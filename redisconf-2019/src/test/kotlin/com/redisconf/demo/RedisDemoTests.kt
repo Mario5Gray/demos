ec6bbe1eb127d39d06b58499a60301b7b11da765
Mario Gray <mario.gray@gmail.com>
2019-04-03 13:59:43 -0700

    test 3

--- redisconf-2019/src/test/kotlin/com/redisconf/demo/RedisDemoTests.kt
+++ redisconf-2019/src/test/kotlin/com/redisconf/demo/RedisDemoTests.kt
@@ -1,14 +1,19 @@
 package com.redisconf.demo
 
+import org.hamcrest.MatcherAssert
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import org.reactivestreams.Publisher
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
 import redis.embedded.RedisServer
 
 
@@ -42,5 +47,38 @@ class RedisDemoTests {
 
     @AfterAll
     fun tearDown() = redisServer.stop()
+
+    @Test
+    fun testShouldPing() {
+
+        val ping = template.connectionFactory.reactiveConnection.ping()
+
+        StepVerifier
+                .create(ping)
+                .expectSubscription()
+                .expectNext("PONG")
+                .verifyComplete()
+    }
+
+    @Test
+    fun testShouldSetGet() {
+        val cachePut: Publisher<Boolean> =
+                template.opsForValue()
+                        .set("TEST", "1234")
+
+        val cacheGet: Publisher<String> =
+                template.opsForValue()
+                        .get("TEST")
+
+        val setAndGet = Flux.from(cachePut).thenMany(cacheGet)
+
+        StepVerifier
+                .create(setAndGet)
+                .expectSubscription()
+                .assertNext { t ->
+                    MatcherAssert.assertThat("Receives Value for Key", t, idMatcher)
+                }
+                .verifyComplete()
+    }
 
 }
